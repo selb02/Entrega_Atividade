416a333db92986e525ca91aa3ee8a6b89d9effdf
Joao Manoel <100219441+selb02@users.noreply.github.com>
2025-08-20 18:48:55 -0300

    Update and rename api.py to app.py

---
+++ atividade_criar_api/app.py
@@ -0,0 +1,60 @@
+from flask import Flask, jsonify, request
+
+meuApp = Flask(__name__)
+
+Users = []
+
+@meuApp.route('/users', methods=['POST'])
+def create_users():
+    data = request.json
+
+    campos = [
+        'nome', 'email', 
+    ]
+
+    campos_vazio = [campo for campo in campos if not data.get(campo)]
+    if campos_vazio:
+        return jsonify({'mensagem': f'Esses campos são obrigatorios e não podem estar vazios: {", ".join(campos_vazio)}'}), 400
+    
+    user = {'id': len(Users) + 1, 'nome': data['nome'], 'email': data['email']}
+    Users.append(user)
+    return jsonify(user), 201
+
+@meuApp.route('/users', methods=['GET'])
+def get_users():
+    return jsonify({'Users': Users}), 200
+
+@meuApp.route('/users/<int:user_id>', methods=["GET"])
+def get_user(user_id):
+    for user in Users:
+        if user['id'] == user_id:
+            return jsonify(user), 200
+    return jsonify({'mensagem': 'Aluno não encontrado'}), 404
+
+@meuApp.route('/users/<int:user_id>', methods=["PUT"])
+def update_user(user_id):    
+    for user in Users:
+        if user['id'] == user_id:
+            dados = request.json
+        
+        campos = [
+            'nome', 'email'
+        ]
+
+        for campo in campos:
+            if campo in dados:
+                user[campo] = dados[campo]
+        return jsonify(user), 200
+    return jsonify({'mensagem': 'User não encontrado'}), 404
+
+@meuApp.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    for user in Users:
+        if user['id'] == user_id:
+            Users.remove(user)
+            return jsonify({'mensagem': 'User removido'}), 200
+    return jsonify({'mensagem': 'User não encontrado'}), 404
+
+
+if __name__ == '__main__':
+    meuApp.run(debug=True)
